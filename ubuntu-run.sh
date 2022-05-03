08a6249497e954ace844351aa8aed58a0c479d22
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2022-05-03 13:10:03 +0000

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -3,7 +3,9 @@ sudo apt-get update --fix-missing
 sudo apt-get install git wget
 sudo wget https://storage.googleapis.com/git-repo-downloads/repo
 sudo mv repo /bin
-sudo chmod a+x /bin/repo 
+sudo chmod a+x /bin/repo
+git config --global user.email "gamerabous094@gmail.com"
+git config --global user.name "AndVer0"
 repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-10.0 
 repo sync
 sudo apt-get install bc bison build-essential zstd clang flex gnupg gperf ccache liblz4-tool libncurses5-dev libsdl1.2-dev libstdc++6 libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zlib1g-dev libncurses5-dev bzip2 git gcc g++ libssl-dev openssl -y && sudo apt-get install gcc-aarch64-linux-gnu && sudo apt-get install gcc-arm-linux-gnueabihf && sudo apt-get install lld && sudo apt-get install gcc-arm-linux-gnueabi && sudo apt-get install llvm
