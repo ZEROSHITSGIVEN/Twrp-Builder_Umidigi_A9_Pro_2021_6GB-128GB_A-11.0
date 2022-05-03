29ec2d5414f4d9b1b1ffffb8708cf7c41ca5bbee
AndVer0 <68866362+AndVer0@users.noreply.github.com>
2022-05-03 15:38:39 +0000

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -3,4 +3,4 @@ sudo apt-get update --fix-missing
 sudo apt-get install git ccache automake flex lzop bison gperf build-essential zip curl zlib1g-dev  g++-multilib libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven libssl-dev pwgen libswitch-perl policycoreutils minicom libxml-sax-base-perl libxml-simple-perl bc libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev xsltproc unzip 
 git config --global user.email "gamerabous094@gmail.com"
 git config --global user.name "AndVer0"
-cd && sudo mkdir all && cd all && git clone https://github.com/AndVer0/samsung-stockkernel-m105g-androidQ-M10GDXU7CUF2 && mv sam* kernel && cd /opt && git clone https://github.com/Shubhamvis98/toolchains && cd && cd all/kernel 
+cd && sudo mkdir all && cd all && sudo git clone https://github.com/AndVer0/samsung-stockkernel-m105g-androidQ-M10GDXU7CUF2 && sudo mv sam* kernel && cd /opt && sudo git clone https://github.com/Shubhamvis98/toolchains && cd && cd all/kernel 
